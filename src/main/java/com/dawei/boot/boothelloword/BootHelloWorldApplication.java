cbee725ebca99d39ba7b0c65f80b13adeec69055
zhaoqianze <zhaoqianze@100tal.com>
2020-03-20 12:25:52 +0800

    添加cloud配置，降低sentinel版本兼容cloud版本

--- src/main/java/com/dawei/boot/boothelloword/BootHelloWorldApplication.java
+++ src/main/java/com/dawei/boot/boothelloword/BootHelloWorldApplication.java
@@ -3,12 +3,17 @@ package com.dawei.boot.boothelloword;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 //Mybatis 扫包
 //@MapperScan(basePackages = "com.dawei.boot.boothelloword.mapper")
+@EnableEurekaClient
+@EnableFeignClients
 public class BootHelloWorldApplication {
 
     public static void main(String[] args) {
